657c7e47dc74e90edbf5e79cd4d61002fec9d112
architest <23104729+architest@users.noreply.github.com>
2018-06-29 18:09:41 +0200

    Modify to adjust it to Pypi requirements.

--- setup.py
+++ setup.py
@@ -3,10 +3,30 @@ try:
 except ImportError:
     from distutils.core import setup
 
+from os import path
+import pypandoc
+
+
+# Converts Markdown to reStructured
+z = pypandoc.convert('README.md', 'rst', format='markdown')
+
+# Writes converted file
+with open('README.rst', 'w') as outfile:
+    outfile.write(z)
+
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+
 config = {
     'name': 'PyMeeus',
     'version': '0.0.1',
     'description': 'Python implementation of Jean Meeus astronomical routines',
+    'long_description': long_description,
     'keywords': 'Meeus astronomy module library',
     'license': 'LGPLv3',
     'author': 'Dagoberto Salazar',
@@ -14,8 +34,7 @@ config = {
     'url': 'https://github.com/architest/pymeeus',
     'download_url': 'https://github.com/architest/pymeeus',
     'install_requires': ['nose'],
-    'packages': ['pymeeus'],
-    'scripts': ['example.py'],
+    'packages': ['pymeeus'],            # 'scripts': ['example.py'],
     'py_modules': ['base'],
     'classifiers': [
         'Development Status :: 3 - Alpha',
@@ -23,10 +42,10 @@ config = {
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU Lesser General Public License v3',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
         'Operating System :: OS Independent',
-        'Programming Language :: Pythoni :: 2.7',
-        'Programming Language :: Pythoni :: 3.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering :: Astronomy'
     ]
 }
