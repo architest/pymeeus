9420c965c9d248e787a4fd6194def1f18a084679
architest <23104729+architest@users.noreply.github.com>
2018-07-10 18:36:33 +0200

    Add CurveFitting class and improve documentation

--- pymeeus/base.py
+++ pymeeus/base.py
@@ -18,7 +18,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-from math import sqrt, pi, degrees, radians
+from math import sqrt, pi, degrees, radians, fsum
 
 
 """
@@ -1018,15 +1018,15 @@ class Interpolation(object):
 
     :note: When using Angles, be careful with the 360-to-0 discontinuity.
 
-    If a sequence of int/floats is given, the values in the odd positions are
-    considered to belong to the 'x' set, while the values in the even positions
-    belong to the 'y' set. If only one tuple or list is provided, it is assumed
-    that it is the 'y' set, and the 'x' set is build from 0 onwards with steps
-    of length 1.
+    If a sequence of int, floats or Angles is given, the values in the odd
+    positions are considered to belong to the 'x' set, while the values in the
+    even positions belong to the 'y' set. If only one tuple or list is
+    provided, it is assumed that it is the 'y' set, and the 'x' set is build
+    from 0 onwards with steps of length 1.
 
     Please keep in mind that a minimum of two data pairs are needed in order to
-    carry out any interpolation. If only one value is provided, a ValueError
-    exception will be raised.
+    carry out any interpolation. If only one value pair is provided, a
+    ValueError exception will be raised.
     """
 
     def __init__(self, *args):
@@ -1045,8 +1045,8 @@ class Interpolation(object):
         build from 0 onwards with steps of length 1.
 
         Please keep in mind that a minimum of two data pairs are needed in
-        order to carry out any interpolation. If only one value is provided, a
-        ValueError exception will be raised.
+        order to carry out any interpolation. If only one value pair is
+        provided, a ValueError exception will be raised.
 
         :param \*args: Input tabular values.
         :type \*args: int, float, list, tuple, Angle
@@ -1078,7 +1078,7 @@ class Interpolation(object):
         self.set(*args)         # Let's use 'set()' method to handle the setup
 
     def _order_points(self):
-        """Method to put in ascending order (w.r.t. 'x') the data points."""
+        """Method to put the data points in ascending order w.r.t. 'x'."""
 
         # Let's work with copies of the original lists
         x = list(self._x)
@@ -1121,8 +1121,8 @@ class Interpolation(object):
         build from 0 onwards with steps of length 1.
 
         Please keep in mind that a minimum of two data pairs are needed in
-        order to carry out any interpolation. If only one value is provided, a
-        ValueError exception will be raised.
+        order to carry out any interpolation. If only one value pair is
+        provided, a ValueError exception will be raised.
 
         :param \*args: Input tabular values.
         :type \*args: int, float, list, tuple, Angle
@@ -1503,6 +1503,262 @@ class Interpolation(object):
         return prime.root(xl, xh, max_iter)
 
 
+class CurveFitting(object):
+    """
+    Class CurveFitting deals with finding the function (linear, cuadratic, etc)
+    that best fit a given set of points.
+
+    The constructor takes pairs of (x, y) values from the table of interest.
+    These pairs of values can be given as a sequence of int/floats, tuples,
+    lists or Angles.
+
+    :note: When using Angles, be careful with the 360-to-0 discontinuity.
+
+    If a sequence of int, floats or Angles is given, the values in the odd
+    positions are considered to belong to the 'x' set, while the values in the
+    even positions belong to the 'y' set. If only one tuple or list is
+    provided, it is assumed that it is the 'y' set, and the 'x' set is build
+    from 0 onwards with steps of length 1.
+
+    Please keep in mind that a minimum of two data pairs are needed in order to
+    carry out any fitting. If only one value pair is provided, a ValueError
+    exception will be raised.
+    """
+
+    def __init__(self, *args):
+        """Interpolation constructor.
+
+        This takes pairs of (x, y) values from the table of interest. These
+        pairs of values can be given as a sequence of int/floats, tuples, lists
+        or Angles.
+
+        :note: When using Angles, be careful with the 360-to-0 discontinuity.
+
+        If a sequence of int, floats or Angles is given, the values in the odd
+        positions are considered to belong to the 'x' set, while the values in
+        the even positions belong to the 'y' set. If only one tuple or list is
+        provided, it is assumed that it is the 'y' set, and the 'x' set is
+        build from 0 onwards with steps of length 1.
+
+        Please keep in mind that a minimum of two data pairs are needed in
+        order to carry out any interpolation. If only one value pair is
+        provided, a ValueError exception will be raised.
+
+        :param \*args: Input tabular values.
+        :type \*args: int, float, list, tuple, Angle
+
+        :returns: Interpolation object.
+        :rtype: Interpolation
+        :raises: ValueError if not enough input data pairs are provided.
+        :raises: TypeError if input values are of wrong type.
+
+        >>> i = CurveFitting([5, 3, 6, 1, 2, 4, 9], [10, 6, 12, 2, 4, 8])
+        >>> print(i._x)
+        [5, 3, 6, 1, 2, 4, 9]
+        >>> print(i._y)
+        [10, 6, 12, 2, 4, 8]
+        >>> j = CurveFitting([3, -8, 1, 12, 2, 5, 8])
+        >>> print(j._x)
+        [0, 1, 2, 3, 4, 5, 6]
+        >>> print(j._y)
+        [3, -8, 1, 12, 2, 5, 8]
+        >>> k = CurveFitting(3, -8, 1, 12, 2, 5, 8)
+        >>> print(k._x)
+        [3, 1, 2]
+        >>> print(k._y)
+        [-8, 12, 5]
+        """
+        # Initialize data table
+        self._x = []
+        self._y = []
+        self.set(*args)         # Let's use 'set()' method to handle the setup
+
+    def set(self, *args):
+        """Method used to define the value pairs of Interpolation object.
+
+        This takes pairs of (x, y) values from the table of interest. These
+        pairs of values can be given as a sequence of int/floats, tuples,
+        lists, or Angles.
+
+        :note: When using Angles, be careful with the 360-to-0 discontinuity.
+
+        If a sequence of int, floats or Angles is given, the values in the odd
+        positions are considered to belong to the 'x' set, while the values in
+        the even positions belong to the 'y' set. If only one tuple or list is
+        provided, it is assumed that it is the 'y' set, and the 'x' set is
+        build from 0 onwards with steps of length 1.
+
+        Please keep in mind that a minimum of two data pairs are needed in
+        order to carry out any interpolation. If only one value is provided, a
+        ValueError exception will be raised.
+
+        :param \*args: Input tabular values.
+        :type \*args: int, float, list, tuple, Angle
+
+        :returns: None.
+        :rtype: None
+        :raises: ValueError if not enough input data pairs are provided.
+        :raises: TypeError if input values are of wrong type.
+
+        >>> i = CurveFitting()
+        >>> i.set([5, 3, 6, 1, 2, 4, 9], [10, 6, 12, 2, 4, 8])
+        >>> print(i._x)
+        [5, 3, 6, 1, 2, 4, 9]
+        >>> print(i._y)
+        [10, 6, 12, 2, 4, 8]
+        >>> j = CurveFitting()
+        >>> j.set([3, -8, 1, 12, 2, 5, 8])
+        >>> print(j._x)
+        [0, 1, 2, 3, 4, 5, 6]
+        >>> print(j._y)
+        [3, -8, 1, 12, 2, 5, 8]
+        >>> k = CurveFitting(3, -8, 1, 12, 2, 5, 8)
+        >>> print(k._x)
+        [3, 1, 2]
+        >>> print(k._y)
+        [-8, 12, 5]
+        """
+        # Clean up the internal data tables and parameters
+        self._x = []
+        self._y = []
+        # If no arguments are given, return. Internal data tables are empty
+        if len(args) == 0:
+            return
+        # If we have only one argument, it can be a single value or tuple/list
+        elif len(args) == 1:
+            if isinstance(args[0], (int, float, Angle)):
+                # Insuficient data for curve fitting. Raise ValueError
+                raise ValueError("Invalid number of input values")
+            elif isinstance(args[0], (list, tuple)):
+                seq = args[0]
+                if len(seq) < 2:
+                    raise ValueError("Invalid number of input values")
+                else:
+                    # Read input values into 'y', and create 'x'
+                    i = 0
+                    for value in seq:
+                        self._x.append(i)
+                        self._y.append(value)
+                        i += 1
+            else:
+                raise TypeError("Invalid input value")
+        elif len(args) == 2:
+            if isinstance(args[0], (int, float, Angle)) or \
+                    isinstance(args[1], (int, float, Angle)):
+                # Insuficient data for curve fitting. Raise ValueError
+                raise ValueError("Invalid number of input values")
+            elif isinstance(args[0], (list, tuple)) and \
+                    isinstance(args[1], (list, tuple)):
+                x = args[0]
+                y = args[1]
+                # Check if they have the same length. If not, make them equal
+                length_min = min(len(x), len(y))
+                x = x[:length_min]
+                y = y[:length_min]
+                if len(x) < 2 or len(y) < 2:
+                    raise ValueError("Invalid number of input values")
+                else:
+                    # Read input values into 'x' and 'y'
+                    for xval, yval in zip(x, y):
+                        self._x.append(xval)
+                        self._y.append(yval)
+            else:
+                raise TypeError("Invalid input value")
+        elif len(args) == 3:
+            # In this case, no combination of input values is valid
+            raise ValueError("Invalid number of input values")
+        else:
+            # If there is an odd number of arguments, drop the last one
+            if len(args) % 2 != 0:
+                args = args[:-1]
+            # Check that all the arguments are ints, floats or Angles
+            all_numbers = True
+            for arg in args:
+                all_numbers = all_numbers and \
+                            isinstance(arg, (int, float, Angle))
+            # If any of the values failed the test, raise an exception
+            if not all_numbers:
+                raise TypeError("Invalid input value")
+            # Now, extract the data: Odds are x's, evens are y's
+            for i in range(int(len(args)/2.0)):
+                self._x.append(args[2 * i])
+                self._y.append(args[2 * i + 1])
+        # Compute parameters
+        if len(self._x) > 0:
+            self._compute_parameters()
+
+    def _compute_parameters(self):
+        """Method to compute the intermediate parameters using for fitting."""
+        self._P = 0.0
+        self._Q = 0.0
+        self._R = 0.0
+        self._S = 0.0
+        self._T = 0.0
+        self._U = 0.0
+        self._V = 0.0
+        self._W = 0.0
+        self._N = len(self._x)
+        self._P = fsum(self._x)
+        self._T = fsum(self._y)
+        for i in range(self._N):
+            x2 = self._x[i] * self._x[i]
+            xy = self._x[i] * self._y[i]
+            self._Q += x2
+            self._R += (x2 * self._x[i])
+            self._S += (x2 * x2)
+            self._U += xy
+            self._V += (xy * self._x[i])
+            self._W += (self._y[i] * self._y[i])
+        return
+
+    def __str__(self):
+        """Method used when trying to print the object.
+
+        :returns: Internal tabular values as strings.
+        :rtype: string
+
+        >>> i = CurveFitting([5, 3, 6, 1, 2, 4, 9], [10, 6, 12, 2, 4, 8])
+        >>> print(i)
+        X: [5, 3, 6, 1, 2, 4, 9]
+        Y: [10, 6, 12, 2, 4, 8]
+        """
+        xstr = "X: " + str(self._x) + "\n"
+        ystr = "Y: " + str(self._y)
+        return xstr + ystr
+
+    def correlation_coeff(self):
+        """This method returns the coefficient of correlation, as a float.
+
+        :returns: Coefficient of correlation.
+        :rtype: float
+        """
+        n = self._N
+        sxy = self._U
+        sx = self._P
+        sy = self._T
+        sx2 = self._Q
+        sy2 = self._W
+        return (n*sxy - sx*sy)/(sqrt(n*sx2 - sx*sx)*sqrt(n*sy2 - sy*sy))
+
+    def linear_fitting(self):
+        """This method returns a tuple with the 'a', 'b' coefficients of of the
+        linear equation 'y = a*x + b' that best fits the table data, using the
+        least squares approach.
+
+        :returns: 'a', 'b' coefficients of best linear equation fit.
+        :rtype: tuple
+        """
+        n = self._N
+        sxy = self._U
+        sx = self._P
+        sy = self._T
+        sx2 = self._Q
+        d = n*sx2 - sx*sx
+        a = (n*sxy - sx*sy)/d
+        b = (sy*sx2 - sx*sxy)/d
+        return (a, b)
+
+
 def main():
 
     # Let's define a small helper function
@@ -1832,6 +2088,23 @@ def main():
     print_me("sin'(30.0)\t", degrees(m.derivative(30.0)))
     print_me("sqrt(3.0)/2.0\t", sqrt(3.0)/2.0)
     print_me("sin'(30.5)\t", degrees(m.derivative(30.5)))
+
+    # Let's now work with the CurveFitting class
+    print('\n' + 35*'*')
+    print("*** Use of CurveFitting class")
+    print(35*'*' + '\n')
+
+    cf = CurveFitting([73.0, 38.0, 35.0, 42.0, 78.0, 68.0, 74.0, 42.0, 52.0,
+                       54.0, 39.0, 61.0, 42.0, 49.0, 50.0, 62.0, 44.0, 39.0,
+                       43.0, 54.0, 44.0, 37.0],
+                      [90.4, 125.3, 161.8, 143.4, 52.5, 50.8, 71.5, 152.8,
+                       131.3, 98.5, 144.8, 78.1, 89.5, 63.9, 112.1, 82.0,
+                       119.8, 161.2, 208.4, 111.6, 167.1, 162.1])
+
+    a, b = cf.linear_fitting()
+    print("a = {}\tb = {}".format(round(a, 2), round(b, 2)))
+    r = cf.correlation_coeff()
+    print_me("r", round(r, 3))
 
 
 if __name__ == '__main__':
