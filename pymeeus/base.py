d7d8e50ce6a9d0479514cdcf91a90b4efb8aa605
architest <23104729+architest@users.noreply.github.com>
2018-07-13 10:27:59 +0200

    Add 'machine_accuracy()' function and example of use

--- pymeeus/base.py
+++ pymeeus/base.py
@@ -31,6 +31,22 @@ TOL = 1E-10
 """Internal tolerance being used by default"""
 
 
+def machine_accuracy():
+    """This function computes the accuracy of the computer being used.
+
+    :returns: A tuple containing the number of significant bits in the mantissa
+    of a floating number, and the number of significant digits in a decimal
+    number.
+    :rtype: tuple
+    """
+    j = 0.0
+    x = 2.0
+    while x + 1.0 != x:
+        j += 1.0
+        x *= 2.0
+    return (j, int(j*0.30103))
+
+
 def main():
 
     # Let's define a small helper function
@@ -39,6 +55,11 @@ def main():
 
     # Let's print the tolerance
     print_me("The default value for the tolerance is", TOL)
+
+    # Find the accuracy of this computer
+    j, d = machine_accuracy()
+    print_me("Number of significant BITS in the mantissa\t", j)
+    print_me("Number of significant DIGITS in a decimal number", d)
 
 
 if __name__ == '__main__':
