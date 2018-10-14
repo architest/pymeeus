a98265d842546c7d3989fad6ba9577df2660ff46
architest <23104729+architest@users.noreply.github.com>
2018-10-14 15:06:55 +0200

    Add methods 'rectangular_coordinates_mean_equinox()' and 'rectangular_coordinates_J2000()'

--- pymeeus/Sun.py
+++ pymeeus/Sun.py
@@ -247,6 +247,102 @@ class Sun(object):
         lat = -lat
         return lon, lat, r
 
+    @staticmethod
+    def rectangular_coordinates_mean_equinox(epoch):
+        """"This method computes the rectangular geocentric equatorial
+        coordinates (X, Y, Z) of the Sun, referred to the mean equinox of the
+        data. The X axis is directed towards the vernal equinox (longitude 0),
+        the Y axis lies in the plane of the equator and is directed towards
+        longitude 90, and the Z axis is directed towards the north celestial
+        pole.
+
+        :param epoch: Epoch to compute Sun position, as an Epoch object
+        :type epoch: :py:class:`Epoch`
+
+        :returns: A tuple with the X, Y, Z values in astronomical units
+        :rtype: tuple
+        :raises: TypeError if input values are of wrong type.
+
+        >>> epoch = Epoch(1992, 10, 13.0)
+        >>> x, y, z = Sun.rectangular_coordinates_mean_equinox(epoch)
+        >>> print(round(x, 7))
+        -0.9380032
+        >>> print(round(y, 6))
+        -0.311634
+        >>> print(round(z, 7))
+        -0.1351127
+        """
+
+        # NOTE: In page 172, Meeus gives a different value for the LONGITUDE
+        # (199.907347 degrees) as the one internally computed in the example
+        # above (199.905991 degrees). After many checks and tests, I came to
+        # the conclusion that the results above are the right one, and Meeus'
+        # results are wrong
+
+        # First check that input values are of correct types
+        if not isinstance(epoch, Epoch):
+            raise TypeError("Invalid input types")
+        # Get Sun's geocentric position with reduction to FK5 system
+        lon, lat, r = Sun.geometric_geocentric_position(epoch)
+        # Get the mean obliquity
+        epsilon0 = mean_obliquity(epoch)
+        # Change to radians
+        ll = lon.rad()
+        b = lat.rad()
+        e = epsilon0.rad()
+        # Compute the results
+        x = r*cos(ll)
+        y = r*(sin(ll)*cos(e) - sin(b)*sin(e))
+        z = r*(sin(ll)*sin(e) + sin(b)*cos(e))
+        return x, y, z
+
+    @staticmethod
+    def rectangular_coordinates_J2000(epoch, to_FK5=True):
+        """"This method computes the rectangular geocentric equatorial
+        coordinates (X, Y, Z) of the Sun, referred to the standard equinox of
+        J2000.0. The X axis is directed towards the vernal equinox (longitude
+        0), the Y axis lies in the plane of the equator and is directed towards
+        longitude 90, and the Z axis is directed towards the north celestial
+        pole.
+
+        :param epoch: Epoch to compute Sun position, as an Epoch object
+        :type epoch: :py:class:`Epoch`
+        :param to_FK5: Whether or not the small correction to convert to FK5
+            system will be applied or not
+        :type to_FK5: bool
+
+        :returns: A tuple with the X, Y, Z values in astronomical units
+        :rtype: tuple
+        :raises: TypeError if input values are of wrong type.
+
+        >>> epoch = Epoch(1992, 10, 13.0)
+        >>> x, y, z = Sun.rectangular_coordinates_J2000(epoch, to_FK5=False)
+        >>> print(round(x, 8))
+        -0.93740382
+        >>> print(round(y, 8))
+        -0.34131407
+        >>> print(round(z, 8))
+        -3.85e-06
+        """
+
+        # First check that input values are of correct types
+        if not isinstance(epoch, Epoch):
+            raise TypeError("Invalid input types")
+        # Second, compute Earth heliocentric position referred to J2000.0
+        lon, lat, r = Earth.geometric_heliocentric_position_J2000(epoch,
+                                                                  toFK5=False)
+        # Third, convert from Earth's heliocentric to Sun's geocentric
+        lon = lon.to_positive() + 180.0
+        lat = -lat
+        x = r*cos(lat.rad())*cos(lon.rad())
+        y = r*cos(lat.rad())*sin(lon.rad())
+        z = r*sin(lat.rad())
+        if to_FK5:
+            x += 0.00000044036*y - 0.000000190919*z
+            y = -0.000000479966*x + 0.917482137087*y - 0.397776982902*z
+            z = 0.397776982902*y + 0.917482137087*z
+        return x, y, z
+
 
 def main():
 
