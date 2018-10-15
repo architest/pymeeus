068ca6da197777d3042746f223e0ff64ec37f520
architest <23104729+architest@users.noreply.github.com>
2018-10-15 21:50:36 +0200

    Add minor improvements to method 'rectangular_coordinates_J2000()'

--- pymeeus/Sun.py
+++ pymeeus/Sun.py
@@ -297,7 +297,7 @@ class Sun(object):
         return x, y, z
 
     @staticmethod
-    def rectangular_coordinates_J2000(epoch, to_FK5=True):
+    def rectangular_coordinates_J2000(epoch):
         """"This method computes the rectangular geocentric equatorial
         coordinates (X, Y, Z) of the Sun, referred to the standard equinox of
         J2000.0. The X axis is directed towards the vernal equinox (longitude
@@ -307,40 +307,35 @@ class Sun(object):
 
         :param epoch: Epoch to compute Sun position, as an Epoch object
         :type epoch: :py:class:`Epoch`
-        :param to_FK5: Whether or not the small correction to convert to FK5
-            system will be applied or not
-        :type to_FK5: bool
 
         :returns: A tuple with the X, Y, Z values in astronomical units
         :rtype: tuple
         :raises: TypeError if input values are of wrong type.
 
         >>> epoch = Epoch(1992, 10, 13.0)
-        >>> x, y, z = Sun.rectangular_coordinates_J2000(epoch, to_FK5=False)
+        >>> x, y, z = Sun.rectangular_coordinates_J2000(epoch)
         >>> print(round(x, 8))
-        -0.93740382
+        -0.93740412
         >>> print(round(y, 8))
-        -0.34131407
+        -0.31314716
         >>> print(round(z, 8))
-        -3.85e-06
+        -0.12456636
         """
 
         # First check that input values are of correct types
         if not isinstance(epoch, Epoch):
             raise TypeError("Invalid input types")
         # Second, compute Earth heliocentric position referred to J2000.0
-        lon, lat, r = Earth.geometric_heliocentric_position_J2000(epoch,
-                                                                  toFK5=False)
+        lon, lat, r = Earth.geometric_heliocentric_position_J2000(epoch)
         # Third, convert from Earth's heliocentric to Sun's geocentric
         lon = lon.to_positive() + 180.0
         lat = -lat
         x = r*cos(lat.rad())*cos(lon.rad())
         y = r*cos(lat.rad())*sin(lon.rad())
         z = r*sin(lat.rad())
-        if to_FK5:
-            x += 0.00000044036*y - 0.000000190919*z
-            y = -0.000000479966*x + 0.917482137087*y - 0.397776982902*z
-            z = 0.397776982902*y + 0.917482137087*z
+        x += 0.00000044036*y - 0.000000190919*z
+        y = -0.000000479966*x + 0.917482137087*y - 0.397776982902*z
+        z = 0.397776982902*y + 0.917482137087*z
         return x, y, z
 
 
