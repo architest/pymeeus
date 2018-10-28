003b16fed0b2486e9335da5bee1281b4f04b4400
architest <23104729+architest@users.noreply.github.com>
2018-10-28 13:52:50 +0100

    Add tests for 'Jupiter' class

---
+++ tests/test_jupiter.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+
+# PyMeeus: Python module implementing astronomical algorithms.
+# Copyright (C) 2018  Dagoberto Salazar
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+from pymeeus.base import TOL
+from pymeeus.Jupiter import Jupiter
+from pymeeus.Epoch import Epoch
+
+
+# Jupiter class
+
+def test_jupiter_geometric_heliocentric_position():
+    """Tests the geometric_heliocentric_position() method of Jupiter class"""
+
+    epoch = Epoch(2018, 10, 27.0)
+    lon, lat, r = Jupiter.geometric_heliocentric_position(epoch)
+
+    assert abs(round(lon.to_positive(), 4) - 241.5873) < TOL, \
+        "ERROR: 1st geometric_heliocentric_position() test doesn't match"
+
+    assert abs(round(lat, 4) - 0.8216) < TOL, \
+        "ERROR: 2nd geometric_heliocentric_position() test doesn't match"
+
+    assert abs(round(r, 5) - 5.36848) < TOL, \
+        "ERROR: 3rd geometric_heliocentric_position() test doesn't match"
