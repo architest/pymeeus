16c6e9b421d6eecb9436a7deeb98bbe07b15e633
architest <23104729+architest@users.noreply.github.com>
2018-11-27 14:27:19 +0100

    Fix line too long

--- setup.py
+++ setup.py
@@ -14,32 +14,32 @@ with open(path.join(here, 'README.rst')) as f:
 
 
 config = {
-    'name': 'PyMeeus',
-    'version': '0.2.10',
-    'description': 'Python implementation of Jean Meeus astronomical routines',
-    'long_description': long_description,
-    'keywords': 'Meeus astronomy module library',
-    'license': 'LGPLv3',
-    'author': 'Dagoberto Salazar',
-    'author_email': 'dagoberto.salazar@gmail.com',
-    'url': 'https://github.com/architest/pymeeus',
-    'download_url': 'https://github.com/architest/pymeeus',
-    # 'install_requires': ['nose', 'pypandoc'],
-    'packages': ['pymeeus'],
-    # 'scripts': ['example.py'],
-    'py_modules': ['base'],
-    'classifiers': [
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Scientific/Engineering :: Astronomy'
-    ]
+  'name': 'PyMeeus',
+  'version': '0.2.10',
+  'description': 'Python implementation of Jean Meeus astronomical routines',
+  'long_description': long_description,
+  'keywords': 'Meeus astronomy module library',
+  'license': 'LGPLv3',
+  'author': 'Dagoberto Salazar',
+  'author_email': 'dagoberto.salazar@gmail.com',
+  'url': 'https://github.com/architest/pymeeus',
+  'download_url': 'https://github.com/architest/pymeeus',
+  # 'install_requires': ['nose', 'pypandoc'],
+  'packages': ['pymeeus'],
+  # 'scripts': ['example.py'],
+  'py_modules': ['base'],
+  'classifiers': [
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Scientific/Engineering :: Astronomy'
+  ]
 }
 
 setup(**config)
