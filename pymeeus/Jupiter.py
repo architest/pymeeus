231ff3156830411bbffb5686195a69ba3927f694
architest <23104729+architest@users.noreply.github.com>
2018-12-14 12:43:04 +0100

    Add example of use of method 'geocentric_position()' for 'Mars' class

--- pymeeus/Jupiter.py
+++ pymeeus/Jupiter.py
@@ -4091,6 +4091,15 @@ def main():
 
     print("")
 
+    # Compute the geocentric position for 1992/12/20:
+    epoch = Epoch(1992, 12, 20.0)
+    ra, dec, elon = Jupiter.geocentric_position(epoch)
+    print_me("Right ascension", ra.ra_str(n_dec=1))
+    print_me("Declination", dec.dms_str(n_dec=1))
+    print_me("Elongation", elon.dms_str(n_dec=1))
+
+    print("")
+
     # Print mean orbital elements for Jupiter at 2065.6.24
     epoch = Epoch(2065, 6, 24.0)
     l, a, e, i, ome, arg = Jupiter.orbital_elements_mean_equinox(epoch)
