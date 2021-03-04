0165b2fba61f8bbeb10818020df0ae3bdd5380f3
architest <23104729+architest@users.noreply.github.com>
2021-03-04 20:02:32 +0100

    Added file 'make.bat'

---
+++ docs/make.bat
@@ -0,0 +1,41 @@
+@ECHO OFF
+
+pushd %~dp0
+
+REM Command file for Sphinx documentation
+
+if "%SPHINXBUILD%" == "" (
+	set SPHINXBUILD=sphinx-build
+)
+set SOURCEDIR=source
+set BUILDDIR=build
+set GENERATEDDIR=generated
+
+if "%1" == "" goto help
+
+%SPHINXBUILD% >NUL 2>NUL
+if errorlevel 9009 (
+	echo.
+	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
+	echo.installed, then set the SPHINXBUILD environment variable to point
+	echo.to the full path of the 'sphinx-build' executable. Alternatively you
+	echo.may add the Sphinx directory to PATH.
+	echo.
+	echo.If you don't have Sphinx installed, grab it from
+	echo.http://sphinx-doc.org/
+	exit /b 1
+)
+
+if not exist %GENERATEDDIR%\ (
+    mkdir .\generated
+)
+REM Run external tools to generate documentation artefacts.
+REM pandoc -s ..\README.md -o .\%GENERATEDDIR%\README.md.rst
+%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+goto end
+
+:help
+%SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+
+:end
+popd
